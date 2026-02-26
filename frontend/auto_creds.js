0c233049eeb69e56d705ac12051a5d64e828004d
dounia-benamer-blip <Dounia-Benamer@hotmail.fr>
2026-02-26 22:19:33 +0000

    auto-commit for 4cf18514-996a-4b4a-87af-2d9ccbceb879

--- frontend/auto_creds.js
+++ frontend/auto_creds.js
@@ -1,5 +1,4 @@
-const { spawn } = require('child_process');
-const readline = require('readline');
+const pty = require('node-pty');
 
 const env = {
     ...process.env,
@@ -7,78 +6,98 @@ const env = {
     EXPO_ASC_KEY_ID: 'RHS5L7DYW4',
     EXPO_ASC_ISSUER_ID: '44554a2e-a243-4574-b6be-b436ae95c91d',
     EXPO_ASC_API_KEY_PATH: '/app/frontend/AuthKey_RHS5L7DYW4.p8',
-    EXPO_APPLE_TEAM_ID: 'MNPJV87Q6P'
+    EXPO_APPLE_TEAM_ID: 'MNPJV87Q6P',
+    TERM: 'xterm-256color'
 };
 
-const proc = spawn('eas', ['credentials', '--platform', 'ios'], {
+const proc = pty.spawn('eas', ['credentials', '--platform', 'ios'], {
+    name: 'xterm-256color',
+    cols: 120,
+    rows: 40,
     cwd: '/app/frontend',
-    env: env,
-    stdio: ['pipe', 'pipe', 'pipe']
+    env: env
 });
 
 let outputBuffer = '';
+let promptCount = 0;
 
-proc.stdout.on('data', (data) => {
-    const str = data.toString();
-    outputBuffer += str;
-    console.log('STDOUT:', str);
+proc.onData((data) => {
+    outputBuffer += data;
+    process.stdout.write(data);
     
-    // Auto-respond based on prompts
-    if (str.includes('Which build profile')) {
-        // Select production (send down arrow twice, then enter)
-        setTimeout(() => {
-            proc.stdin.write('\x1b[B');  // Down
+    // Build profile selection - we want production (3rd option)
+    if (data.includes('Which build profile') && !outputBuffer.includes('production')) {
+        promptCount++;
+        if (promptCount === 1) {
             setTimeout(() => {
-                proc.stdin.write('\x1b[B');  // Down
+                proc.write('\x1b[B');  // Down arrow
                 setTimeout(() => {
-                    proc.stdin.write('\n');
-                }, 200);
-            }, 200);
-        }, 500);
+                    proc.write('\x1b[B');  // Down arrow
+                    setTimeout(() => {
+                        proc.write('\r');  // Enter
+                    }, 300);
+                }, 300);
+            }, 1000);
+        }
     }
     
-    if (str.includes('Apple Team Type') || str.includes('team type')) {
-        // Select Individual (3rd option)
+    // Team type - Individual (3rd option)
+    if (data.includes('Apple Team Type')) {
         setTimeout(() => {
-            proc.stdin.write('\x1b[B');
+            proc.write('\x1b[B');
             setTimeout(() => {
-                proc.stdin.write('\x1b[B');
+                proc.write('\x1b[B');
                 setTimeout(() => {
-                    proc.stdin.write('\n');
-                }, 200);
-            }, 200);
+                    proc.write('\r');
+                }, 300);
+            }, 300);
+        }, 1000);
+    }
+    
+    // Login prompt
+    if (data.includes('Do you want to log in') || data.includes('log in to your Apple')) {
+        setTimeout(() => {
+            proc.write('Y\r');
         }, 500);
     }
     
-    if (str.includes('log in to your Apple account')) {
+    // What do you want to do - select first (Set up)
+    if (data.includes('What do you want to do')) {
         setTimeout(() => {
-            proc.stdin.write('Y\n');
+            proc.write('\r');
         }, 500);
     }
     
-    if (str.includes('What do you want to do')) {
+    // Distribution Certificate options
+    if (data.includes('Distribution Certificate')) {
         setTimeout(() => {
-            proc.stdin.write('\n');  // Select first option
+            proc.write('\r');  // Select first option
         }, 500);
     }
     
-    if (str.includes('Generate new') || str.includes('Create a new')) {
+    // Generate new
+    if (data.includes('Generate new') || data.includes('generate a new')) {
         setTimeout(() => {
-            proc.stdin.write('\n');
+            proc.write('\r');
+        }, 500);
+    }
+    
+    // Provisioning Profile
+    if (data.includes('Provisioning Profile')) {
+        setTimeout(() => {
+            proc.write('\r');
         }, 500);
     }
 });
 
-proc.stderr.on('data', (data) => {
-    console.log('STDERR:', data.toString());
+proc.onExit(({ exitCode }) => {
+    console.log('\n\nProcess exited with code:', exitCode);
+    process.exit(exitCode);
 });
 
-proc.on('close', (code) => {
-    console.log('Process exited with code:', code);
-});
-
-// Timeout after 2 minutes
+// Timeout after 3 minutes
 setTimeout(() => {
-    console.log('Timeout - killing process');
+    console.log('\n\nTimeout reached - stopping');
     proc.kill();
-}, 120000);
+    process.exit(1);
+}, 180000);
